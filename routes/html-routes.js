e6a123bc0411e3181c1b1279b55e308fb7c6373b
good morning <172862789+jr-jpg@users.noreply.github.com>
2024-09-15 18:25:29 -0500

    Created HTML routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,4 +1,14 @@
-// GET /notes should return the notes.html file
+const path = require('path');
+const html = require('express').Router();
 
+// GET /notes should return the notes.html file
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, 'public/routes.html'))
+);
 
 // GET * should return the index.html file
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+
+module.exports = html;
