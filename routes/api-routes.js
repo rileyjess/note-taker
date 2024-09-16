6ee05386aeb9faeb52aa46d20a6451b9860f03e1
good morning <172862789+jr-jpg@users.noreply.github.com>
2024-09-16 16:49:55 -0500

    Created API routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,5 +1,47 @@
+const apiRoute = requrie('express').Router();
+const fs = require('fs');
+
+// npm package to create a unique id
+const uuid = require('uuid');
+
 // GET /api/notes should read the db.json file and return all saved notes as JSON
+apiRoute.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json')
+        .then((data) => res.json(JSON.parse(data)));
+});
 
 // POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client
+apiRoute.post('/api/notes', (req, res) => {
+    fs.readFile('../db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.log(err)
+        } else {
+            const parsedNotes = JSON.parse(data);
 
-// give each note a unique id when it's saved - use an npm package
+            const newNote = {
+                title: req.body.title,
+                text: req.body.text,
+                // give each note a unique id when it's saved - use an npm package
+                id: uuid(),
+            };
+
+            parsedNotes.push(newNote);
+
+            fs.writeFile('../db/db.json', JSON.stringify(parsedNotes));
+        }
+    })
+});
+
+// Bonus: DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete
+apiRoute.delete('/api/notes/:id', (req, res) => {
+    // read all notes from the db.json file
+    fs.readFile('../db/db.json', 'utf8', (data) => {
+        const notes = JSON.parse(data);
+
+        // remove the note with the given id property
+        removeNote = notes.filter(note => note.id !== req.params.id)
+
+        // rewrite the notes to the db.json file
+        fs.writeFile('../db/db.json', JSON.stringify(removeNote));
+    })
+});
